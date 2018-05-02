1200612075ca76df11a43e98ec6a992fc3d3dd34
Bhanoo Gulati <bhanoo.gulati@gmail.com>
2018-05-02 09:14:07 -0400

    commiting

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+
+const router = require("express").Router();
+
+const houses = require("./houses");
+const users = require("./user");
+const houseUserRelationship = require("./house-user-relationship");
+
+module.exports = router;
