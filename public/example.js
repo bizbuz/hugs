58e26b0a3b7c8269a9e1a3a53b16e7193155e204
Charles W. Roberts <charles@localhost.localdomain>
2018-05-03 14:08:49 -0400

    Create example front page

--- public/example.js
+++ public/example.js
@@ -4,19 +4,24 @@ function AddClick(event)
 {
 	console.log("I'm in AddClick");
 	$.ajax({url: 'api/addItem', type: 'GET', 
-		data: {"name":"milk","quantity":"1","upcCode":"","note":"","marked":false,"imageURL":""}}).
+		data: {"houseID":"1234","name":"milk","quantity":"1","upcCode":"","note":"","marked":false,"imageURL":""}}).
 
       then (function(result){
         console.log("Did addItem");
        
-      });
+      }).
+      catch()
+      {
+      	console.log("There was an error");
+      };
+
 }
 
 function EditClick(event)
 {
 	console.log("I'm in EditClick");
 	$.ajax({url: 'api/editItem', type: 'GET', 
-		data: {"name":"milk","quantity":"2","upcCode":"","note":"","marked":false,"imageURL":""}}).
+		data: {"houseID":"1234","name":"milk","quantity":"2","upcCode":"","note":"","marked":false,"imageURL":""}}).
 
       then (function(result){
         console.log("Did editItem");
@@ -33,12 +38,16 @@ function DeleteClick(event)
 {
 	console.log("I'm in DeleteClick");
 	$.ajax({url: 'api/deleteItem', type: 'GET', 
-		data: {"name":"milk","quantity":"2","upcCode":"","note":"","marked":false,"imageURL":""}}).
+		data: {"houseID":"1234","name":"milk","quantity":"2","upcCode":"","note":"","marked":false,"imageURL":""}}).
 
       then (function(result){
         console.log("Did deleteItem");
        
-      });
+      }).
+      catch()
+      {
+      	console.log("There was an error");
+      };
 
 }
 
